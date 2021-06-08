1cde72a841c80f918aa146dc529074db72776bbb
marcostl2 <marcossilvatl2@gmail.com>
2021-06-08 23:55:36 +0000

    dqdw

---
+++ index.c
@@ -0,0 +1,23 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+  int p = 5, q = 11, e = 7;
+  int n = p * q;
+
+  printf("\nN: %d\n", n);
+
+  int qN = (p - 1) * (q - 1);
+
+  int invMultiplicativo
+
+  printf("\nqN %d\n", qN);
+
+  // printf("\n%f\n", pow(qN, -1));
+
+  char name[] = "VINICIUS";
+
+  return 0;
+}
